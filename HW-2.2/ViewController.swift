50013786dcb099b4144862419d52be025601f038
Nureles Abdullaev <nureles.abdullaiuulu.ss@gmail.com>
2024-04-26 18:46:04 -0500

    Initial commit

---
+++ HW-2.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW-2.2
+//
+//  Created by Nureles Abdullai uulu on 4/26/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
